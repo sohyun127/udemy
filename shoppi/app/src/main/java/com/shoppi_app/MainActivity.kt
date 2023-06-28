9f949bda7deedf6f138e0e1ad30a1e31d841301f
Sohyun <so405@naver.com>
2023-06-28 11:36:04 +0900

    [FEAT] 하단 탭 구성

--- shoppi/app/src/main/java/com/shoppi_app/MainActivity.kt
+++ shoppi/app/src/main/java/com/shoppi_app/MainActivity.kt
@@ -2,6 +2,8 @@ package com.shoppi_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +13,13 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
         bottomNavigationView.itemIconTintList = null
+
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
     }
+
 
 
 }
